a3bdd8c92acd361f646264d50a8662c76b91fe9e
imac <773999579@qq.com>
2016-10-28 21:49:03 +0800

    init

---
+++ src/MD5Hasher.php
@@ -0,0 +1,21 @@
+<?php
+namespace FirstShuai\Hasher;
+/**
+ * Created by PhpStorm.
+ * User: FirstShuai
+ * Date: 2016/10/28
+ * Time: 下午3:09
+ */
+class MD5Hasher
+{
+    public function make($value, array $options = [])
+    {
+        $salt = isset($options['salt']) ? $options['salt'] : '';
+        return md5($value.$salt);
+    }
+    public function check($value,$hasherValer,array $options = [])
+    {
+        $salt = isset($options['salt']) ? $options['salt'] : '';
+        return $hasherValer === md5($value.$salt);
+    }
+}
---
+++ src/MD5HasherProvider.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace FirstShuai\Hasher;
+
+use Illuminate\Support\ServiceProvider;
+
+class MD5HasherProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->singleton('md5hasher',function (){
+            return new MD5Hasher();
+        });
+    }
+}
