1afb8206d95b71838be80d1fc43e2f811ab59fc4
imac <773999579@qq.com>
2016-10-30 21:31:13 +0800

    重新配置phpunit环境

---
+++ tests/MD5HasherTest.php
@@ -0,0 +1,24 @@
+<?php
+use PHPUnit\Framework\TestCase;
+use FirstShuai\Hasher\MD5Hasher;
+
+/**
+ * Created by PhpStorm.
+ * User: FirstShuai
+ * Date: 2016/10/30
+ * Time: 下午9:25
+ */
+class MD5HasherTest extends TestCase
+{
+    protected $hasher;
+    public function setUp()
+    {
+        $this->hasher = new MD5Hasher();
+    }
+    public function testMD5HasherMake()
+    {
+        $passwordOne = md5('123456');
+        $passwordTwo = $this->hasher->make('123456');
+        $this->assertEquals($passwordOne,$passwordTwo);
+    }
+}
